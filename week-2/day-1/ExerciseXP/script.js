761fc3ab377e913376049771a0fcb5cffa18f7a8
dyschaf <schaffeldovid@gmail.com>
2022-06-26 10:29:53 +0300

    week2 day1

--- week-2/day-1/ExerciseXP/script.js
+++ week-2/day-1/ExerciseXP/script.js
@@ -1,3 +1,163 @@
-let birthYear = 1997;
-let futureYear = 2072;
-console.log(`"I will be ${futureYear-birthYear} in ${futureYear}'`)
+// Exercise 1: Your Favorite Food
+let favoriteFood = 'iceCream';
+let favoriteMeal = 'lunch';
+console.log(`"I like ${favoriteFood} at every ${favoriteMeal}"`);
+// Exercise 2: Series
+let myWatchedSeries = ['black mirror','money heist', 'The big bang theory'];
+let myWatchedSeriesLength = myWatchedSeries.length;
+let myWatchedSeriesSentence = myWatchedSeries;
+console.log(`"I watch ${myWatchedSeriesLength}: ${myWatchedSeriesSentence}`);
+myWatchedSeries[2]= 'friends';
+myWatchedSeries.push ='24'
+console.log(myWatchedSeries)
+delete myWatchedSeries[0];
+console.log(myWatchedSeries)
+console.log(myWatchedSeries[1][2]);
+// Exercise 3 : The Temperature Converter
+// Exercise 4 : Guess The Answers #1
+
+//1
+let c;
+let a = 34;
+let b = 21;
+console.log(a+b);
+// Perdiction 55
+// Actual 55
+
+//2
+a = 2;
+console.log(a+b);
+// Perdiction 23
+// Actual 23
+
+// 3
+console.log(c)
+// Perdiction undefine
+// Actual undefined
+
+console.log(3 + 4 + '5');
+// Perdiction 75
+// Actual 75
+
+// Exercise 5 : Guess The Answers #2
+
+console.log(typeof(15));
+// Prediction:num
+// Actual:num
+
+console.log(typeof(5.5));
+// Prediction:num
+// Actual:num
+
+console.log(typeof(NaN));
+// Prediction:undefined
+// Actual: number
+
+console.log(typeof("hello"));
+// Prediction: string
+// Actual:srting
+
+console.log(typeof(true));
+// Prediction: boolean
+// Actual:boolean
+
+console.log(typeof(1 != 2));
+// Prediction:boolean
+// Actual:boolean
+
+console.log("hamburger" + "s");
+// Prediction:hamburgers
+// Actual:hamburgers
+
+console.log("hamburgers" - "s");
+// Prediction:hamburger
+// Actual:nan
+
+console.log("1" + "3");
+// Prediction:13
+// Actual:13
+
+console.log("1" - "3");
+// Prediction:undefined
+// Actual:-2
+
+console.log("johnny" + 5);
+// Prediction:jonny5
+// Actual:jonny5
+
+console.log("johnny" - 5);
+// Prediction:undefined
+// Actual:nan
+
+console.log(99 * "hello");
+// Prediction:undefined
+// Actual:nan
+
+console.log(1 != 1);
+// Prediction:false
+// Actual:false
+
+console.log(1 == "1");
+// Prediction: false
+// Actual:
+true
+console.log(1 === "1");
+// Prediction:false
+// Actual:false
+
+// Exercise 6 : Guess The Answers #3
+console.log(5 + "34");
+// Prediction:534
+// Actual:534
+
+console.log(5 - "4");
+// Prediction:1
+// Actual:1
+
+console.log(10 % 5);
+// Prediction:0
+// Actual:0
+
+console.log(5 % 10);
+// Prediction:0
+// Actual:5
+
+console.log("Java" + "Script");
+// Prediction:JavaScript
+// Actual:JavaScript
+
+console.log(" " + " ");
+// Prediction:  
+// Actual:  
+
+console.log(" " + 0);
+// Prediction: 0
+// Actual: 0
+
+console.log(true + true);
+// Prediction:true
+// Actual:2
+
+console.log(true + false);
+// Prediction:false
+// Actual:1
+
+console.log(false + true);
+// Prediction:false
+// Actual:1
+
+console.log(false - true);
+// Prediction:false
+// Actual:-1
+
+console.log(!true);
+// Prediction:false
+// Actual:false
+
+console.log(3 - 4);
+// Prediction:-1
+// Actual:-1
+
+console.log("Bob" - "bill");
+// Prediction:nan
+// Actual:nan
