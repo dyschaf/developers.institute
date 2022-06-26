fff5a8bf948db81c266b66413d88f6ffe180b79b
dyschaf <schaffeldovid@gmail.com>
2022-06-26 10:24:23 +0300

    week2 day1

---
+++ week-2/day-1/DailyChallenge/script.js
@@ -0,0 +1,3 @@
+let birthYear = 1997;
+let futureYear = 2072;
+console.log(`"I will be ${futureYear-birthYear} in ${futureYear}'`)
---
+++ week-2/day-1/script1.js
@@ -0,0 +1,4 @@
+let addressNumber = 5;
+let addressStreet = "ben yehuda";
+let country = "israel";
+console.log(`"I live in ${addressStreet} ${addressNumber} ${country} "`);
